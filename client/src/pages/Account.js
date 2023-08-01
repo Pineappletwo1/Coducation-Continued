e573f34cfebe753038f7e0bfdecea382e92c9a98
Max Locke <lockemaximus@yahoo.com>
2023-07-31 17:55:50 -0700

    you better work heroku

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -14,7 +14,7 @@ export default function Account() {
       } else {
         document.querySelectorAll("img")[1].src =
           "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-        alert("Image cannot be found!");
+        alert("Image cannot be found!!!");
       }
     };
   }
